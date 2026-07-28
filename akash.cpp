fe4a1ec3018d57196e74a5dcbf04f9756c5ab197
Akash Soni <aksoni.5157@gmail.com>
2026-07-28 15:33:15 +0530

    initial commit

---
+++ akash.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+using namespace std;
+int sum(int a,int b) // function example
+{
+   // int c;
+  //  c=a+b;
+   // return c;
+
+}
+
+
+
+int main()
+
+
+{
+    // cout<<"hello world";
+    // short sa=40;
+    // cout<<sa;
+
+    /*camelcase notation
+     int marksInMaths=85;
+     cout<<"the marks in maths of student rahul "<<marksInMaths;*/
+    
+   /*int a; // sum , difference , multiply and divide of two numbers
+    float b;
+    cout<<"enter a number"<<endl;
+    cin>>a;
+     cout<<"enter b number"<<endl;
+     cin>>b;
+     cout<<"a+b "<< a+b<<endl;
+     cout<<"a-b "<< a-b<<endl;
+     cout<<"a*b "<< a*b<<endl;
+     cout<<"a/b "<< a/b<<endl; */
+
+    /* int age; // if else if statement example 
+     cout<<"enter your age"<<endl;
+     cin>>age;
+    if (age>150 || age<1)        
+    {
+        cout<<"invalid age"<<endl;
+
+    } else if (age>=18)
+    {
+       cout<<" you are elligable for vote"<<endl;
+    }
+    
+    
+    else{
+        cout<<"you are not elligable for vote"<<endl;
+    }*/
+
+    /* int age ; switch case statement example
+     cout<<"enter your age"<<endl;
+     cin>>age;
+     switch (age)  
+     {
+     case 12:
+         cout<<"you are 12 year old";
+        break;
+        case 18:
+         cout<<"you are 18 year old";
+        break;
+        case 20:
+         cout<<"you are 20 year old";
+        break;
+        case 25:
+         cout<<"you are 25 year old";
+        break;
+     
+     default:
+     cout<<"age not mentiond";
+        break;
+     } */   
+    
+// loops exampla
+   /* int  a,b=1;// multiplication table any table like 2,4,8....etc
+    cout<<"enter a number "<<endl;
+     cin>>a;
+     cout<<endl;
+
+     
+    do
+    {
+        cout<< a*b <<endl;
+        b=b+1;
+
+
+    } while (b<=10);*/
+
+   /* int a; // counting start user need to 100 (under 100)
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    cout<<endl;
+    
+    while(a<=100)
+    {
+        cout<<a+1<<endl;
+        a=a+1;
+    }*/
+    
+     /*int a,b=0;// counting start with 0 to user need 
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    cout<<endl;
+    do
+    {
+        cout<<b<<endl;
+        b=b+1;
+    } while (b<=a);*/
+    
+  /*  int a; // using foor loop print a table
+    cout<<"enter a number"<<endl;
+    cin>>a;
+
+
+    for ( int i=1;i<=10; i++)
+    {
+        cout<<a*i<<endl;
+    }
+        */
+    
+//int a,b; // function program 
+//cout<<"enter a number"<<endl;
+//cin>>a;
+//cout<<"enter b number"<<endl;
+//cin>>b;
+//cout<<"the function return is "<<sum(a,b);
+  
+  
+   // int arr[5]={1 ,5, 7 ,4 ,6};
+// array index={0  1  2  3   4 }
+//cout<<arr[4];
+/*int marks[7];
+for (int i = 1; i <=6; i++)
+{
+    cout<<"enter the marks of"<<i<<"th student"<<endl;
+    cin>>marks[i];
+
+}
+for (int i = 1; i <=6; i++){
+cout<<" marks is"<<i<<"th student is "<<marks[i]<<endl;}
+
+
+
+
+       return 0;*/
+
+
+      /* int a,sum; // sum od digits code
+       sum=0;
+       cout<<"enter a number";
+       cin>>a;
+       while (a>0)
+       {
+        sum=sum+(a%10);
+        a=a/10;
+       }
+       cout<<"sum of digits" <<sum; */
+       
+
+     /*  int a,b,c; // factorial of a number 
+       a=1;
+       b=1;
+       cout<<"enter a number =";
+       cin>>c;
+       while ( b<=c)
+       {
+        a=a*b;
+        b=b+1;
+       }
+       cout<<"factorial is "<<a; */
+     
+       
+   /* int a,b,c,n; // fibonacci series
+    a=0;
+    b=1;
+    cout<< "enter number of terms";
+    cin>>n;
+    for (int i = 0; i<n; i++)
+    {
+         cout<<a<<" ";
+        c=a+b;
+    a=b;
+    b=c;
+    
+ } */
+    
+
+/* char ch; // FIND CHARACTER IS UPPER CASR OR LOWER CASE // (A-Z = 67-90) (a-z = 97-122) =ASCII VALUE
+ cout<<"enter character";
+ cin>>ch;
+ if (ch>=67&& ch<=90) // implicit type conversion
+ {
+    cout<<"the character is UPER  case";
+ }else
+ cout<<"the character is LOWER  case"; */
+ 
+
+ /*int n; // print a number positive or negative
+ cout<<"enter a number";
+ cin>>n;
+ cout<<(n>=0? "positive":"negative"); // ternary statement */
+
+
+ 
+    }
+
+
+
+
+
+
+
+    
---
+++ dsain.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int n; // factorial of n numbers
+    // int a=1;
+    // int i=1;
+    // cout<<"enter the value n"<<endl;
+    // cin>>n;
+    // while (i<=n)
+    // {
+
+        // a=a*i;
+        // i+=1;
+   
+   
+    // }
+    // cout<<"the factorial is"<<a<<endl;
+    
+    // return 0;
+
+
+//int a,b; // max od two numbers
+//cout<<"enter a number a"<<endl;
+//cin>>a;
+//cout<<"enter a number b"<<endl;
+//cin>>b;
+//if (a>b)
+//{
+   // cout<<"the max is "<<a<<endl;
+//}else
+ //cout<<"the max is "<<b<<endl;
+
+
+ //float P; // simple interest problem 
+//float T;
+ //float R;
+ //float SI;
+ //char unit;
+// cout<<"enter the principal"<<endl;
+// cin>>P;
+ //cout<<"enter the rate"<<endl;
+// cin>>R;
+// cout<<"enter the time"<<endl;
+// cin>>T;
+// cout<<"enter unit (y for year,m for month,d for days)";
+// cin>>unit;
+// if ( unit=='m' || unit=='M')
+// {
+  // T=T/12;
+// }else if (unit=='d' || unit=='D')
+// {
+  //  T=T/365;
+// }
+
+//SI=P*R*T/100;
+ // cout<<"simple interest is "<<SI<<endl;
+
+
+ //int a; // even or odd problem
+ //cout<<"enter a number";
+ //cin>>a;
+ //if (a%2==0)
+// {
+ // cout<<"number id even";
+
+// }else{
+ // cout<<"number is odd";
+
+
+//int a,b,c; // max of three number 
+//cout<<"enter a number";
+//cin>>a;
+//cout<<"enter b number";
+//cin>>b;
+//cout<<"enter c number";
+//cin>>c;
+//if (a>b && a>c)
+//{
+ // cout<<"a is max";
+
+//}else if (b>c && b>a)
+//{
+  //cout<<"b is max";
+//}else {
+ // cout<<"c is max";
+
+//}
+
+
+//int year; //leap year problem 
+//cout<<"enter a year";
+//cin>>year;
+//if (year%4==0 && year%100!=0 || year%400==0)
+//{
+ // cout<<"this is leap yaer";
+//}else{
+  //cout<<"not a leap year";
+//}
+
+
+//int marks; // grading system
+//cout<<"enter your marks";
+//cin>>marks;
+//if (marks>=90)
+//{
+ // cout<<"your grade is A+";
+//}else if (marks>=80)
+//{
+  // cout<<"your grade is A";
+//}else if (marks>=70)
+//{
+ // cout<<"your grade is B";
+//}else {
+ // cout<<"your grade is C";
+//}
+
+
+/*float a,b; // calculator 
+int choice;
+cout<<"enter a number";
+cin>>a;
+cout<<"enter b number";
+cin>>b;
+cout<<"enter the choice (add for1 ,subtract for 2,multiply for 3,divide for 4,modulas for 5)";
+cin>>choice;
+switch (choice)
+{
+case 1:
+  cout<<"result is ="<<a+b;
+  break;
+  case 2:
+  cout<<"result is ="<<a-b;
+  break;
+  case 3:
+   cout<<"result is ="<<a*b;
+  break;
+  case 4:
+  cout<<"result is ="<<a/b;
+  break;
+ // case 5:
+   //cout<<"result is ="<<a%b; // float k sath modulas kaam nhi krta 
+  break;
+default:
+  break;*/
+
+
+/*int n=0; // example of ternary statement 
+cout<<"enter a number";
+cin>>n;
+cout<<(n>=0 ? "positive" : "negative"); // ternary statement */
+
+
+/*int n,sum,i; // SUM OF N NUMBERS 
+sum=0;
+cout<<"enter a number";
+cin>>n;
+for ( i =1 ; i <= n; i++)
+
+  sum=sum+i;
+{  
+  cout<<"the sum is = "<<sum<<endl;
+}*/
+
+
+/*int n,i,sum=0; //sum of 1to n odd number
+cout<<"enter a number =";
+cin>>n;
+for ( i = 1; i<=n; i++)
+if (i%2!=0)
+sum+=i;
+{
+  //sum+=i;
+  cout<<"the sum of odd number ="<<sum<<endl;
+}*/
+
+
+
+}
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
